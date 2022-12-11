2960d3c3817ba391186d6f8d5fca6c8a921e9fa0
Hunter Hodnett <hchodnett@gmail.com>
2022-12-11 13:16:27 -0600

    Delete replit.nix

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-	deps = [
-		pkgs.sudo
-  pkgs.nodejs-18_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-	];
-}
